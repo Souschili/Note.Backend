643122ff416a8a68d13a274cc0ea8623ccb6e6b0
OrHaN_1982 <aliyevorhan82@gmail.com>
2022-05-08 04:16:08 +0200

    Add context?initializer

---
+++ Notes.Persistence/DbInitializer.cs
@@ -0,0 +1,11 @@
+﻿namespace Notes.Persistence
+{
+    public class DbInitializer
+    {
+        // init for fist app running
+        public static void Initialize(NotesDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}

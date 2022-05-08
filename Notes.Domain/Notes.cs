9e511ba0678e470b37459ccf71e8a24977a4fcd9
OrHaN_1982 <aliyevorhan82@gmail.com>
2022-05-08 03:39:36 +0200

    Add project files.

---
+++ Notes.Domain/Notes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Notes.Domain
+{
+    public class Notes
+    {
+        public Guid UserId  { get; set; }
+        public Guid Id  { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime? EditedAt { get; set; }
+        public DateTime? CreationDate { get; set; }
+
+    }
+}

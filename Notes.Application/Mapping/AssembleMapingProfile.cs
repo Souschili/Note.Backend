e7ff6620ce344f2b099a1c6be0e2ad4184bf0544
OrHaN_1982 <aliyevorhan82@gmail.com>
2022-05-08 05:44:59 +0200

    implenent autommaper from assembly

---
+++ Notes.Application/Mapping/AssembleMapingProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Notes.Application.Mapping
+{
+    public class AssembleMapingProfile : Profile
+    {
+
+        public AssembleMapingProfile(Assembly assembly) =>
+            ApplyMapingFromAssembly(assembly);
+
+
+        public void ApplyMapingFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                .Where(type => type.GetInterfaces()
+                .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+                .ToList();
+
+            foreach (var type in types)
+            {
+                var instance=Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+
+
+        }
+    }
+}

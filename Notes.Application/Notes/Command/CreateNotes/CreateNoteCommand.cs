4b2b6230055de157ccd76c9b7a7c224e079ed909
OrHaN_1982 <aliyevorhan82@gmail.com>
2022-05-09 02:36:44 +0200

    Add mediator command and commandhandler

---
+++ Notes.Application/Notes/Command/CreateNotes/CreateNoteCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+
+namespace Notes.Application.Notes.Command.CreateNotes
+{
+    internal class CreateNoteCommand : IRequest<Guid>
+    {
+        public Guid UserId { get; set; }
+        public string Title { get; set; }
+        public string Detailes { get; set; }
+
+    }
+}
---
+++ Notes.Application/Notes/Command/CreateNotes/CreateNoteCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Notes.Domain;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Notes.Application.Notes.Command.CreateNotes
+{
+    internal class CreateNoteCommandHandler : IRequestHandler<CreateNoteCommand, Guid>
+    {
+        public Task<Guid> Handle(CreateNoteCommand request, CancellationToken cancellationToken)
+        {
+            var note = new Note
+            {
+
+            };
+            return null; // temp
+        }
+    }
+}
